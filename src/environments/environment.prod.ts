d4bce9d91ca969f1dab2ffaa6654711366b568ce
lorenz <lorenz.bre@gmail.com>
2020-06-07 14:42:29 +0200

    Set compatibility to prod building

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBL75CKs331Xg_pArBGICbaJphNuLsKJNY',
+    authDomain: 'firechat-95f34.firebaseapp.com',
+    databaseURL: 'https://firechat-95f34.firebaseio.com',
+    projectId: 'firechat-95f34',
+    storageBucket: 'firechat-95f34.appspot.com',
+    messagingSenderId: '752358748146',
+    appId: '1:752358748146:web:c114911fbb08a50faf056b'
+
+  }
 };
